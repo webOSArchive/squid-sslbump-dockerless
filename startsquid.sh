43fd849ae80945d85fdd2e5041cd6fc5739beec0
codepoet <jon.wise@jonandnic.com>
2022-07-14 14:56:05 -0400

    Update startsquid.sh

--- startsquid.sh
+++ startsquid.sh
@@ -3,6 +3,9 @@
 SQUID_USER=squid
 SQUID_DIR=/usr/local/squid
 
+killall squid
+sleep 2
+
 # make sure there's a cert
 if [ ! -f $SQUID_DIR/ssl/localCert.pem ]; then
     openssl req -new -newkey rsa:1024 -nodes -days 3650 -x509 -keyout $SQUID_DIR/ssl/localCert.pem -out $SQUID_DIR/ssl/localCert.crt\
